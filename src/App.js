5ea8c51ccb031d9bbcb1f0a29410501dc32d6fde
Arel <32299070+alitunaarel@users.noreply.github.com>
2020-04-22 14:01:13 +0200

    FEtch Axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
+import axios from "axios";
 
 export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://restcountries.eu/rest/v2/all")
+  //     .then(response => response.json())
+  //     .then(response => setCountries(response));
+  // }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(response => setCountries(response.data))
+      .catch(error => console.log(error));
+  }, []);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start edappen!</h2>
+      {countries.map(country => {
+        return (
+          <div key={country.name}>
+            <h2>{country.name}</h2>
+            <h2>{country.capital}</h2>
+            <p>
+              <img
+                src={country.flag}
+                alt={country.name}
+                style={{ width: "100px" }}
+              />{" "}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
